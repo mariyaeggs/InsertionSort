3de5a49fcf265df396c48612f1b3a5a6176b2b20
mariyaeggs <mariyawilson@live.com>
2017-02-13 13:35:38 -0500

    Add file crendentials.

--- sort_tester.cpp
+++ sort_tester.cpp
@@ -19,6 +19,34 @@ using namespace std;
  * @author Mariya Eggensperger
 */
 
+void PrintArray(int x[], int size);
+void CoolSort(int x[], int size, int sequence[], int sequence_size);
+
 int main() {
+
    return 0;
 }
+/*
+ * Insertion sort algorithms that reads a decreasing
+ * sequence of numbers that end at 1. For each H, it
+ * sorts the sub-arrays that start at arbitrary element(s)
+ * and includes every Hth element using insertion sort.
+ *
+ * @param int x[], int sequence[]
+ * @return void return
+ */
+
+/*
+ * General function that prints the elements
+ * in the selection sort array.
+ *
+ * @param int x[]
+ * @return int x[i]
+ */
+void PrintArray(int x[], int size) {
+   int i;
+   for (i = 0; i < size; i++) {
+      printf("%d ", x[i]);
+   }
+}
+
