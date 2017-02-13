f597115bb9c143c52ebf7314ec2089e051c0474d
mariyaeggs <mariyawilson@live.com>
2017-02-13 11:19:05 -0500

    Initial commit

---
+++ sort_tester.cpp
@@ -0,0 +1,24 @@
+//----- sort_tester.cpp -----
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Title: Program 6 Submission : Insertion Sort
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2
+ *
+ * This is a driver program to test the insertion sort
+ * CoolSort() function. The program reads a decreasing
+ * sequence of numbers that end at 1. For each H, it
+ * sorts the sub-arrays that start at arbitrary element(s)
+ * and includes every Hth element using insertion sort.
+ *
+ * @author Mariya Eggensperger
+*/
+
+int main() {
+   return 0;
+}
